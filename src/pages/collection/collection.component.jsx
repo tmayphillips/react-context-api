4c4f3478e0714d14963769eff598d643a2d0a29b
TIFFANY MAY-PHILLIPS <tiffany.phillips12@gmail.com>
2020-12-02 18:02:09 -0600

    contexts for current user and collections

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useContext } from 'react';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionsContext from '../../contexts/collections/collections.context';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext)
+  const collection = collections[match.params.collectionId]
   const { title, items } = collection;
+  
   return (
     <div className='collection-page'>
       <h2 className='title'>{title}</h2>
@@ -18,11 +20,7 @@ const CollectionPage = ({ collection }) => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
